4d408e697dffa867575f3e3f143bb5602559401a
hykwtakumin <hykwtakumin@gmail.com>
2016-05-23 15:55:34 +0900

    修正

--- samples/chat/cui_chat_client.js
+++ samples/chat/cui_chat_client.js
@@ -6,18 +6,28 @@ var socket = require('socket.io-client').connect('https://pakaruland.herokuapp.c
 var linda = new LindaClient().connect(socket);
 var ts = linda.tuplespace('pakaruland');
 
+var myName = document.getElementById("name").value;
+
+
+
 linda.io.on('connect', function(){
+  myName = document.getElementById("name").value;
+  if (window.localStorage) localStorage.name = myName;
   console.log('connect!!!');
-
   ts.watch({type: "chat"}, function(err, tuple){
-    console.log("> " + tuple.data.message + " (from:" + tuple.from + ")");
+    const user = tuple.data.who;
+    const message = tuple.data.message;
+    if (nameArray.includes(user)) {
+      console.log(user + "<" + message);
+      //document.getElementById(puser).src = "images/l/" + pmessages + ".jpg";
+    }
   });
-
 });
 
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(data){
   ts.write({
+    who: myName,
     type: "chat",
     message: data.replace(/[\r\n]/g, '')
   });
