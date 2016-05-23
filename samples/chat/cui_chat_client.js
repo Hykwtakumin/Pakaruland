d075378e9714cd821fe3b70306d34223ef2b4b52
hykwtakumin <hykwtakumin@gmail.com>
2016-05-23 18:33:24 +0900

    正規表現を追加(殺害予告,爆破予告)

--- samples/chat/cui_chat_client.js
+++ samples/chat/cui_chat_client.js
@@ -6,28 +6,20 @@ var socket = require('socket.io-client').connect('https://pakaruland.herokuapp.c
 var linda = new LindaClient().connect(socket);
 var ts = linda.tuplespace('pakaruland');
 
-var myName = document.getElementById("name").value;
-
-
 
 linda.io.on('connect', function(){
-  myName = document.getElementById("name").value;
-  if (window.localStorage) localStorage.name = myName;
   console.log('connect!!!');
   ts.watch({type: "chat"}, function(err, tuple){
     const user = tuple.data.who;
     const message = tuple.data.message;
-    if (nameArray.includes(user)) {
       console.log(user + "<" + message);
       //document.getElementById(puser).src = "images/l/" + pmessages + ".jpg";
-    }
   });
 });
 
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(data){
   ts.write({
-    who: myName,
     type: "chat",
     message: data.replace(/[\r\n]/g, '')
   });
